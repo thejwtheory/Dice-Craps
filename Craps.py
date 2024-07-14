1026236e2f9aac7fe54299e902cecd0174f0f57f
J.W <73403156+thejwtheory@users.noreply.github.com>
2024-07-13 22:17:13 -0600

    Create Craps.py
    Started off the menu, have to add statements to select game mode + logic

---
+++ Craps.py
@@ -0,0 +1,58 @@
+import os
+
+print("""
+                         .oooooo.                                          
+                        d8P'  `Y8b                                         
+oo.ooooo.  oooo    ooo 888          oooo d8b  .oooo.   oo.ooooo.   .oooo.o 
+ 888' `88b  `88.  .8'  888          `888""8P `P  )88b   888' `88b d88(  "8 
+ 888   888   `88..8'   888           888      .oP"888   888   888 `"Y88b.  
+ 888   888    `888'    `88b    ooo   888     d8(  888   888   888 o.  )88b 
+ 888bod8P'     .8'      `Y8bood8P'  d888b    `Y888""8o  888bod8P' 8""888P' 
+ 888       .o..P'                                       888                
+o888o      `Y8P'                                       o888o             
+  
+                                                                           
+    """)
+
+user = input("Hello welcome to pyCraps! What's your name? ")
+os.system('clear')
+
+question = input(f"""Nice to meet you {user}! What kind of dice game would you like to play? 
+                                                                                       
+                                              ,:::::::;.                                             
+                                          '::::::::::::::::..                                        
+                                       ,:::::::::::::::::::::::.                                     
+                                   ,,::::::::::::::::::::::::::::::.                                 
+                               ^::,::::::::::::::::::::::::::::::::::::                              
+                           ':::::::::::::::::::::::::::::::::::::::::::::::                          
+                         ::::::::::::::::::::::"@@@-(],::::::::::::::::::::,,                        
+                        ,::::::::::::::::::::::$@@@@@@,:::::::::::::::::::,."                        
+                       ^i`.^,::::::::::::::::::::::,:,:::::::::::::::::,`.:ll                        
+                       `ii>l`'",:::::::::::::::::::::::::::::::::::,,^'^IIlll.                       
+                       .iiiiiil"`^,:::::::::::::::::::::::::::::,:"`^;lIkMIll                        
+                        iiiliiiiii;"",::::::::::::::::::::::::,,"";IIlO@@@+ll                        
+                        iid@@@@>iii>il:,:::::::::::::::::::::,,Illlll>@@x+lll                        
+                        i>!](@@@iii>>>>>iI;;::,:::::::::::::;llllllllt@$0;ll:                        
+                        >ii!c@@$iii>>>>>>>>i!lII;;:::;;;;IIllllllllllII!llll.                        
+                        ;>iiiiiiiiiiii>>>>>>>>>ii!;;IIlIlB@@a;lllllll!i@@hlI.                        
+                         iiiiiiiiiiiiiii>>>>>>>>>i;;llll@@@BBllllllllc@@brll                         
+                         iiiiiiiiii>>>iiii>>>>>>>i""lll@@@1>lllllllll$@t0Ill.                        
+                         >iiiiiiii!@ai>>ii>>>>>>>i`'IlI$@@hllllllllll@@;!lll                         
+                         >iiiiiiii@@@@@>>>>>>>>>>i`'IllllIllllllllllllIdIlI:                         
+                         iiiiiiiii],|@@k>>>>>>>>>i`'IllI!@@@>llllllIl@@@@lI                          
+                         >>>>>iiiii<k@@>>>>>>>>>>i^'Ill_@@Wxlllllllld@*"lll                          
+                        ..>>>>>>iii>>>>>>>>>>>>>>i^.Il!@@@I(llllllll@@oll;.                          
+                          '<>>>>>>>>>>>>>>>>>>>>>i^.Ill>@milllllllllllll                             
+                           .^>>>>>>>>>>>>>>>>>>>>i".Illlll@@Illllllll`.                              
+                              .:>>>>>>>>>>@@$_>>>i".Illl@@@@Illllll                                  
+                                  Ii>>>>>iW@@@@>>i".;ll@@@:mllll:.                                   
+                                     l>>>>>;J@@/ii,.;ll$@@!lll.                                      
+                                       .!>i>>bjiii:`IIlllllI                                         
+                                          .l>>iiiiI:IlllI..                                          
+                                              liii!IllI                                              
+                                                                                                     
+                      A) CASINO CRAPS 
+                      B) DICE HEADS
+                      C) GUESS THE DICE ROLL                                                                                                                                        
+                 """)
+                 
