f2866b7d71c1b13d7c29351ffbf3e14add0c182a
J.W <73403156+thejwtheory@users.noreply.github.com>
2024-07-13 23:05:46 -0600

    Update Craps.py
    - Defined dice roll and imaging 
    - Defined basic Craps game rules
    - Defined beginning selection list for start up
    - Added cls  between selections to give it more of a game feel

--- Craps.py
+++ Craps.py
@@ -1,6 +1,78 @@
 import os
+import random
 
-print("""
+def roll_dice():
+    diceone = random.randint(1, 6)
+    dicetw0 = random.randint(1, 6)
+    return diceone, dicetwo
+
+def display_dice(diceone, dicetwo):
+    dice_art = {
+        1: ("+-------+\n"
+            "|       |\n"
+            "|   o   |\n"
+            "|       |\n"
+            "+-------+"),
+        2: ("+-------+\n"
+            "| o     |\n"
+            "|       |\n"
+            "|     o |\n"
+            "+-------+"),
+        3: ("+-------+\n"
+            "| o     |\n"
+            "|   o   |\n"
+            "|     o |\n"
+            "+-------+"),
+        4: ("+-------+\n"
+            "| o   o |\n"
+            "|       |\n"
+            "| o   o |\n"
+            "+-------+"),
+        5: ("+-------+\n"
+            "| o   o |\n"
+            "|   o   |\n"
+            "| o   o |\n"
+            "+-------+"),
+        6: ("+-------+\n"
+            "| o   o |\n"
+            "| o   o |\n"
+            "| o   o |\n"
+            "+-------+"),
+    }
+    return f"{dice_art[diceone]}\n{dice_art[dicetwo]}"
+
+def play_craps():
+    point = None
+    first_roll = True
+    while True:
+        input("Press Enter to roll the dice.")
+        diceone, dicetwo = roll_dice()
+        roll = diceone + dicetwo
+        os.system('cls')
+        print(display_dice(diceone, dicetwo))
+        print(f"You rolled a {roll}")
+
+        if first_roll:
+            if roll in (7, 11):
+                print("You win!")
+                break
+            elif roll in (2, 3, 12):
+                print("Craps! You lose.")
+                break
+            else:
+                point = roll
+                print(f"Your point is {point}")
+                first_roll = False
+        else:
+            if roll == point:
+                print("You win!")
+                break
+            elif roll == 7:
+                print("Seven out! You lose.")
+                break
+
+def main():
+    print("""
                          .oooooo.                                          
                         d8P'  `Y8b                                         
 oo.ooooo.  oooo    ooo 888          oooo d8b  .oooo.   oo.ooooo.   .oooo.o 
@@ -14,10 +86,11 @@ o888o      `Y8P'                                       o888o
                                                                            
     """)
 
-user = input("Hello welcome to pyCraps! What's your name? ")
-os.system('clear')
+    user = input("Hello welcome to pyCraps! What's your name? ")
+    os.system('cls')
 
-question = input(f"""Nice to meet you {user}! What kind of dice game would you like to play? 
+    print(f"Nice to meet you {user}! What kind of dice game would you like to play?")
+    print("""
                                                                                        
                                               ,:::::::;.                                             
                                           '::::::::::::::::..                                        
@@ -32,15 +105,15 @@ question = input(f"""Nice to meet you {user}! What kind of dice game would you l
                        .iiiiiil"`^,:::::::::::::::::::::::::::::,:"`^;lIkMIll                        
                         iiiliiiiii;"",::::::::::::::::::::::::,,"";IIlO@@@+ll                        
                         iid@@@@>iii>il:,:::::::::::::::::::::,,Illlll>@@x+lll                        
-                        i>!](@@@iii>>>>>iI;;::,:::::::::::::;llllllllt@$0;ll:                        
+                        i>!}(@@@iii>>>>>iI;;::,:::::::::::::;llllllllt@$0;ll:                        
                         >ii!c@@$iii>>>>>>>>i!lII;;:::;;;;IIllllllllllII!llll.                        
                         ;>iiiiiiiiiiii>>>>>>>>>ii!;;IIlIlB@@a;lllllll!i@@hlI.                        
                          iiiiiiiiiiiiiii>>>>>>>>>i;;llll@@@BBllllllllc@@brll                         
                          iiiiiiiiii>>>iiii>>>>>>>i""lll@@@1>lllllllll$@t0Ill.                        
                          >iiiiiiii!@ai>>ii>>>>>>>i`'IlI$@@hllllllllll@@;!lll                         
                          >iiiiiiii@@@@@>>>>>>>>>>i`'IllllIllllllllllllIdIlI:                         
-                         iiiiiiiii],|@@k>>>>>>>>>i`'IllI!@@@>llllllIl@@@@lI                          
-                         >>>>>iiiii<k@@>>>>>>>>>>i^'Ill_@@Wxlllllllld@*"lll                          
+                         iiiiiiiii},|@@k>>>>>>>>>i`'IllI!@@@>llllllIl@@@@lI                          
+                         >>>>>iiiii<k@@>>>>>>>>>>i`'Ill_@@Wxlllllllld@*"lll                          
                         ..>>>>>>iii>>>>>>>>>>>>>>i^.Il!@@@I(llllllll@@oll;.                          
                           '<>>>>>>>>>>>>>>>>>>>>>i^.Ill>@milllllllllllll                             
                            .^>>>>>>>>>>>>>>>>>>>>i".Illlll@@Illllllll`.                              
@@ -54,5 +127,20 @@ question = input(f"""Nice to meet you {user}! What kind of dice game would you l
                       A) CASINO CRAPS 
                       B) DICE HEADS
                       C) GUESS THE DICE ROLL                                                                                                                                        
-                 """)
-                 
+    """)
+
+    choice = input("Enter your choice: ").upper()
+    os.system('cls')
+
+    if choice == 'A':
+        print("Welcome to Casino Craps!")
+        play_craps()
+    elif choice == 'B':
+        print("To be added.")
+    elif choice == 'C':
+        print("To be added.")
+    else:
+        print("To be added.")
+
+if __name__ == "__main__":
+    main()
